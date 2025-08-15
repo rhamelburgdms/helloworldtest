5c25bec37314e9f90257b33dea284420ec675a16
rhamelburgdms <rachel.hamelburg@dmsna.com>
2025-08-15 08:57:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.write("Hello World")
+
+blob_name = "Hamelburg Supervisory Hiring_athena.json"
+data = load_json(blob_name)
+
+st.subheader("Raw JSON from Blob")
+st.json(data)  # shows pretty JSON in the app
